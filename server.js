d87a6ef8e3614503cf164b01e445dc53d749602c
TwinkleMedia <hs0052264@gmail.com>
2026-05-22 15:44:27 +0530

    test deploy

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ app.use("/api", certificateRoutes);
 // SERVER
 // ─────────────────────────────────────────────
 const PORT = process.env.PORT || 5000;
+console.log("deploy test");
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
