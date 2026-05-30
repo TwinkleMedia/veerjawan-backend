fcec8980f2900f002a605cfacc9a5cb1cba09b6d
TwinkleMedia <hs0052264@gmail.com>
2026-05-30 15:16:44 +0530

    delect

--- routes/donation.routes.js
+++ routes/donation.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createOrder, getAllDonations,deleteDonation, verifyPayment } from "../controllers/donation.controller.js";
+import { createOrder, getAllDonations, verifyPayment, deleteDonation } from "../controllers/donation.controller.js";
 
 const router = express.Router();
 
 router.post("/create-order", createOrder);
 router.post("/verify-payment", verifyPayment);
 router.get("/all", getAllDonations);
-router.delete("/:id", deleteDonation);
+router.post("/delete/:id", deleteDonation);  // ← POST instead of DELETE
 
 export default router;
